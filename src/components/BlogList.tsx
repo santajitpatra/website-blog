95c641b4239f7c324b70f79e535c18f19bce13b9
SantajitPatra <business.santajitpatra@gmail.com>
2023-04-26 23:25:34 +0530

    add : client side route

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import urlFor from "../../lib/urlFor";
+import ClientSideRoute from "./ClientSideRoute";
 
 type Props = {
   posts: Post[];
@@ -13,59 +14,62 @@ function BlogList({ posts }: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {/* Posts */}
         {posts.map((post) => (
-          <div key={post._id} className="flex flex-col group cursor-pointer ">
-            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
-              <Image
-                className="object-cover object-left lg:object-center"
-                src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                fill
-              />
-              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
-                <div>
-                  <p className="font-bold">{post.title}</p>
-                  <p>
-                    {new Date(post._createdAt).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}
-                  </p>
-                </div>
+          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+            <div className="flex flex-col group cursor-pointer ">
+              <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                <Image
+                  className="object-cover object-left lg:object-center"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  fill
+                />
+                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                  <div>
+                    <p className="font-bold">{post.title}</p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </p>
+                  </div>
 
-                <div className="flex flex-col md:flex-row gap-y-2 md: gap-x-2 items-center">
-                  {post.categories.map((category) => (
-                    <div
-                      key={"item"}
-                      className="bg-[#F7ABOA] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
-                    >
-                      <p>{category.title}</p>
-                    </div>
-                  ))}
+                  <div className="flex flex-col md:flex-row gap-y-2 md: gap-x-2 items-center">
+                    {post.categories.map((category) => (
+                      <div
+                        key={post._id}
+                        className="bg-[#F7ABOA] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                      >
+                        <p>{category.title}</p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
+              <div className="mt-5 flex-1">
+                <p className="underline text-lg font-bold">{post.title}</p>
+                <p className="line-clamp-2 text-gray-500">{post.description}</p>
+              </div>
+              <p className="mt-5 font-bold flex items-center group-hover:underline">
+                Read Post
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="ml-2 w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                  />
+                </svg>
+              </p>
             </div>
-            <div className="mt-5 flex-1">
-              <p className="underline text-lg font-bold">{post.title}</p>
-              <p className="line-clamp-2 text-gray-500">{post.description}</p>
-            </div>
-            <p className="mt-5 font-bold flex items-center group-hover:underline">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="ml-2 w-4 h-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-                />
-              </svg>
-            </p>
-          </div>
+          </ClientSideRoute>
         ))}
       </div>
     </div>
